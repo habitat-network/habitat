3d29545abe0bdea3a4238d3c946c3a5a9090c515
Arushi Bandi <arushibandi@users.noreply.github.com>
2026-03-10 22:21:36 -0700

    actually working

--- typescript/apps/docs/vite.config.ts
+++ typescript/apps/docs/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { devtools } from "@tanstack/devtools-vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
@@ -10,10 +9,6 @@ const config = defineConfig({
     hmr: false, // creates multiple libp2p nodes
   },
   plugins: [
-    devtools({
-      enhancedLogs: { enabled: false },
-      eventBusConfig: { port: parseInt(process.env.DEVTOOLS_PORT ?? "42069", 10) },
-    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
--- typescript/internal/src/habitatAppVitePlugin.ts
+++ typescript/internal/src/habitatAppVitePlugin.ts
@@ -3,6 +3,7 @@ import clientMetadata from "./clientMetadata";
 import type { Plugin } from "vite";
 import util from "node:util";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 const cliArgs = util.parseArgs({
   args: process.argv.slice(process.argv.indexOf("--") + 1),
@@ -33,6 +34,7 @@ export default function habitatAppPlugin(options?: {
   const hashRouting = options?.hashRouting ?? !!process.env.HASH_ROUTING;
 
   return [
+    ...devtools({ eventBusConfig: { port: parseInt(process.env.DEVTOOLS_PORT ?? "42069", 10) } }),
     ...tailwindcss(),
     {
       name: "habitat-app-config",
@@ -49,6 +51,7 @@ export default function habitatAppPlugin(options?: {
           server: {
             host: true,
             allowedHosts: [".ts.net"],
+            port: process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : undefined,
           },
           build: {
             outDir: cliArgs.values.outDir,
