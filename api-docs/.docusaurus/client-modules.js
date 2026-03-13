5f19239629883f743604737bb658706bf6efdf43
arushibandi <9343424+arushibandi@users.noreply.github.com>
2026-03-13 13:04:09 -0700

    [documentation]: fix gh workflow (#223)
    wasn't working because node wasn't being installed properly
    
    also this should be part of our moon project
    
    ---------
    
    Co-authored-by: Arushi Bandi <arushibandi@users.noreply.github.com>

--- api-docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/arushibandi/habitat/habitat/api-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/arushibandi/habitat/habitat/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/arushibandi/habitat/habitat/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_lightningcss@1.31.1_react-dom@19.1._f7a4960d3f44d6fa5db71e1a009f3799/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/arushibandi/habitat/habitat/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.2.7_lightningcss@1.31.1_react-dom@19.1._f7a4960d3f44d6fa5db71e1a009f3799/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/arushibandi/habitat/habitat/api-docs/src/css/custom.css"),
-  require("/Users/arushibandi/habitat/habitat/node_modules/.pnpm/docusaurus-theme-openapi-docs@4.3.4_@docusaurus+theme-common@3.9.2_@docusaurus+plugin-c_e4b8b659d9c27b5f35a8e6bb1ee3ff5e/node_modules/docusaurus-theme-openapi-docs/lib/theme/styles.scss"),
-];
