dfc33fa7c4f712aa0e3d2737e8583b726249ca84
Arushi Bandi <arushibandi@users.noreply.github.com>
2026-03-18 11:42:54 -0700

    build fixes

--- api-docs/src/pages/index.tsx
+++ api-docs/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home(): ReactNode {
-  return <Redirect to={(process.env.BASE_URL ?? '') + "/docs/getting-started"} />;
+  return <Redirect to={useBaseUrl('/docs/getting-started')} />;
 }
