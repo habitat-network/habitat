065e2db510727ed8e7fd2186687468fd4e530651
Arushi Bandi <arushibandi@users.noreply.github.com>
2026-03-31 19:23:49 -0700

    docs outlines

--- api-docs/sidebars.ts
+++ api-docs/sidebars.ts
@@ -5,7 +5,25 @@ const sidebars: SidebarsConfig = {
   docs: [
     {
       type: 'doc',
-      id: 'getting-started',
+      id: 'habitat',
+    },
+    {
+      type: 'category',
+      label: 'Building on habitat',
+      items: [
+        'building/auth',
+        'building/forwarding',
+        'building/permissions',
+        'building/sync',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Specs',
+      items: [
+        'specs/pear',
+        'specs/cliques'
+      ],
     },
     {
       type: 'category',
