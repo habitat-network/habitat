657b6426eb05fb3d85152603ae1ac51df3ab0902
Arushi Bandi <arushibandi@users.noreply.github.com>
2026-04-22 11:32:09 -0700

    create pkg

---
+++ internal/hive/ident.go
@@ -0,0 +1,6 @@
+package hive
+
+// hive roughly stands for "habitat identity verification and enrollment"
+// This package serves org identities based on the DID spec and the did:web: method to be interoperable
+// with the broader AT protocol network, but allow orgs to own their identity minting and management and
+// not rely on the PLC directory as a central source of failure.
