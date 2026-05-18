d1147dabd955aacc3d822d152f3c41c630b75d56
Sashank Gogula <sashankg@gmail.com>
2026-05-18 12:32:01 -0700

    restore client metadata

--- cmd/pear/main.go
+++ cmd/pear/main.go
@@ -128,9 +128,9 @@ func run(_ context.Context, cmd *cli.Command) error {
 
 	domain := cmd.String(fDomain)
 	oauthClient, err := pdsclient.NewPdsOAuthClient(
-		"https://sashankg.github.io/client-metadata.json", /*clientId*/
-		"https://"+domain,                   /*clientUri*/
-		"https://"+domain+"/oauth-callback", /*redirectUri*/
+		"https://"+domain+"/client-metadata.json", /*clientId*/
+		"https://"+domain,                         /*clientUri*/
+		"https://"+domain+"/oauth-callback",       /*redirectUri*/
 		cmd.String(fOauthClientSecret),
 		meter,
 	)
