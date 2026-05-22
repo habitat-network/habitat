22e921ebce934da581f550f83ee4b3a42a7a0e52
Sashank Gogula <sashankg@gmail.com>
2026-05-21 21:33:34 -0700

    [OAuthClient] Publicly host client metadata again

---
+++ cmd/oauth_client_metadata/flags.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/urfave/cli/v3"
+)
+
+var fPearDomain = "pear_domain"
+var fSecret = "secret"
+var fMachineName = "machine_name"
+var fTailscaleDomain = "tailscale_domain"
+
+func getFlags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:     fPearDomain,
+			Required: true,
+			Usage:    "The domain of the pear server",
+			Sources:  cli.EnvVars("HABITAT_DOMAIN"),
+		},
+		&cli.StringFlag{
+			Name:     fSecret,
+			Required: true,
+			Usage:    "The secret of the oauth client",
+			Sources:  cli.EnvVars("HABITAT_OAUTH_CLIENT_SECRET"),
+		},
+		&cli.StringFlag{
+			Name:  fMachineName,
+			Usage: "The machine name of the oauth client",
+			Value: "oauth-client-metadata",
+		},
+		&cli.StringFlag{
+			Name:     fTailscaleDomain,
+			Required: true,
+			Usage:    "The domain of the tailscale server",
+			Sources:  cli.EnvVars("TS_DOMAIN"),
+		},
+	}
+}
--- cmd/pear/flags.go
+++ cmd/pear/flags.go
@@ -24,6 +24,7 @@ var (
 	fHiveDomain         = "hive_domain"
 	fGoogleClientID     = "google_client_id"
 	fGoogleClientSecret = "google_client_secret"
+	fPdsOauthClientUri  = "pds_oauth_client_uri"
 )
 var profiles []string
 
@@ -101,6 +102,11 @@ func getFlags() ([]cli.Flag, []cli.MutuallyExclusiveFlags) {
 				Name:    fGoogleClientSecret,
 				Usage:   "Google OAuth client secret for Google Sign-In login method",
 				Sources: getSources(fGoogleClientSecret),
+			},
+			&cli.StringFlag{
+				Name:    fPdsOauthClientUri,
+				Usage:   "PDS OAuth client ID for PDS login method",
+				Sources: getSources(fPdsOauthClientUri),
 			},
 		}, []cli.MutuallyExclusiveFlags{
 			{
