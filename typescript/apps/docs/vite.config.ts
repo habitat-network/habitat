14b25c4f5a98367e902c3d12496892707885b9da
Sashank Gogula <sashankg@gmail.com>
2026-08-24 08:32:54 -0700

    [Vite] Rm vite-tsconfig-paths plugin

--- typescript/apps/docs/vite.config.ts
+++ typescript/apps/docs/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, mergeConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import habitatAppConfig from "../../vite.config.app";
 
@@ -10,13 +9,9 @@ export default mergeConfig(
     server: {
       hmr: false, // creates multiple libp2p nodes
     },
-    plugins: [
-      // this is the plugin that enables path aliases
-      viteTsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-      tanstackRouter(),
-      viteReact(),
-    ],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    plugins: [tanstackRouter(), viteReact()],
   }),
 );
