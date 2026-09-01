f3053a1df54f07ee3424d4d957a248f6155e6f71
Sashank Gogula <sashankg@gmail.com>
2026-09-01 15:10:32 -0700

    [CI] Fix build

--- internal/opensocial/invite_test.go
+++ internal/opensocial/invite_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/habitat-network/habitat/internal/opensocial"
 	opensocial_testutil "github.com/habitat-network/habitat/internal/opensocial/testutil"
 	"github.com/habitat-network/habitat/internal/spaces"
+	spaces_testutil "github.com/habitat-network/habitat/internal/spaces/testutil"
 	habitat_syntax "github.com/habitat-network/habitat/internal/syntax"
 )
 
@@ -78,7 +79,7 @@ func TestCreateInvite_AlreadyMember(t *testing.T) {
 	_, _, err := spacesStore.PutRecord(
 		t.Context(), membersSpace, orgDID, "community.opensocial.membership",
 		syntax.RecordKey(alice),
-		opensocial_api.CommunityOpensocialMembership{Roles: []string{opensocial.MemberRoleRkey}},
+		spaces_testutil.MustMarshalRecord(t, opensocial_api.CommunityOpensocialMembership{Roles: []string{opensocial.MemberRoleRkey}}),
 	)
 	require.NoError(t, err)
 
@@ -134,7 +135,7 @@ func TestRequestJoin_AlreadyMemberNoInvite(t *testing.T) {
 	_, _, err := spacesStore.PutRecord(
 		t.Context(), membersSpace, orgDID, "community.opensocial.membership",
 		syntax.RecordKey(alice),
-		opensocial_api.CommunityOpensocialMembership{Roles: []string{opensocial.AdminRoleRkey}},
+		spaces_testutil.MustMarshalRecord(t, opensocial_api.CommunityOpensocialMembership{Roles: []string{opensocial.AdminRoleRkey}}),
 	)
 	require.NoError(t, err)
 
